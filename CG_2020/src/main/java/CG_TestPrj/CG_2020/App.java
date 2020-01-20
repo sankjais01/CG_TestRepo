5a844308f7aa74b2209c48d473b7da9abe6eac22
shrsank <shrsank@DESKTOP-4GNS3AA>
2020-01-20 22:22:06 +0530

    test 1st commit for structure

---
+++ CG_2020/src/main/java/CG_TestPrj/CG_2020/App.java
@@ -0,0 +1,13 @@
+package CG_TestPrj.CG_2020;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
